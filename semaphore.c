fe0ab69998285712ee5c5ae66acd2d4c08d85f94
avishagdaniely <avishagdaniely@gmail.com>
2017-05-03 00:35:31 -0700

    with semaphore begginning

---
+++ semaphore.c
@@ -0,0 +1,29 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "x86.h"
+
+type semaphore = record
+value, flag: integer;
+L: list of process;
+end;
+
+void down(S){
+	S.value--;
+	if (S.value < 0) {
+	add this process to S.L;
+	S.flag=0
+	sleep;
+	}
+	else S.flag=0 
+}
+
+
+void up(S){
+	S.value++;
+	if (S.value <= 0) {
+	remove a process P from S.L;
+	wakeup(P);
+	}
+	S.flag=0
+}
